ab7cfd88ad9f3c17d2b5e43a9f7fcd7687785645
ra.saku <xxtiger.of.jiangdongxx@gmail.com>
2018-12-08 11:25:42 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..19).each do |number|
+  Task.create(status: "未着手" + number.to_s, content: 'test content' + number.to_s)
+end
